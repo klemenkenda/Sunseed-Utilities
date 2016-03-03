47237c79ed230e6cbe6034f3aa7895ba38088033
Klemen Kenda <klemen.kenda@ijs.si>
2016-03-03 17:20:58 +0100

    New sensors added + metadata. Idea about syncing http requests.

--- loader/loader.js
+++ loader/loader.js
@@ -15,6 +15,29 @@ if (scriptArgs == "append") {
     db.init();
 }
 
+/* sync object */
+function SyncHttpManager() {
+    this.requests = 0;
+    this.responses = 0;
+
+    function reqMade() {
+        this.requests++;
+    }
+
+    function resReceived() {
+        this.responses++;
+    }
+
+    function isInSync() {
+        return this.requests == this.responses;
+    }
+
+    function stats() {
+        console.log("Requests/responses: " + this.requests + "/" + this.responses);
+    }
+}
+
+var shm = new SyncHttpManager();
 
 /* use a function for the exact format desired... */
 function ISODateString(timestamp) {
@@ -30,8 +53,39 @@ function ISODateString(timestamp) {
 
 function push2QMiner(data) {
     var sensors = [
-        { "name": "f1", "phenomenon": "f", "UoM": "nn" },
-        { "name": "i1", "phenomenon": "i", "UoM": "nn" },
+        { "name": "f1", "phenomenon": "Frequency", "UoM": "Hz" },
+
+        { "name": "i1", "phenomenon": "Current", "UoM": "A" },
+        { "name": "i2", "phenomenon": "Current", "UoM": "A" },
+        { "name": "i3", "phenomenon": "Current", "UoM": "A" },
+        { "name": "i4", "phenomenon": "Current", "UoM": "A" },
+
+        { "name": "pc1", "phenomenon": "Consumed real power", "UoM": "kW" },
+        { "name": "pc2", "phenomenon": "Consumed real power", "UoM": "kW" },
+        { "name": "pc3", "phenomenon": "Consumed real power", "UoM": "kW" },
+
+        { "name": "pg1", "phenomenon": "Generated real power", "UoM": "kW" },
+        { "name": "pg2", "phenomenon": "Generated real power", "UoM": "kW" },
+        { "name": "pg3", "phenomenon": "Generated real power", "UoM": "kW" },
+
+        { "name": "qc1", "phenomenon": "Consumed reactive power", "UoM": "kVAR" },
+        { "name": "qc2", "phenomenon": "Consumed reactive power", "UoM": "kVAR" },
+        { "name": "qc3", "phenomenon": "Consumed reactive power", "UoM": "kVAR" },
+
+        { "name": "qg1", "phenomenon": "Consumed reactive power", "UoM": "kVAR" },
+        { "name": "qg2", "phenomenon": "Consumed reactive power", "UoM": "kVAR" },
+        { "name": "qg3", "phenomenon": "Consumed reactive power", "UoM": "kVAR" },
+
+        { "name": "v1", "phenomenon": "Voltage", "UoM": "V" },
+        { "name": "v2", "phenomenon": "Voltage", "UoM": "V" },
+        { "name": "v3", "phenomenon": "Voltage", "UoM": "V" },
+
+        /*
+         * { "name": "vv1", "phenomenon": "VVoltage", "UoM": "VV" },
+         * { "name": "vv2", "phenomenon": "VVoltage", "UoM": "VV" },
+         * { "name": "vv3", "phenomenon": "VVoltage", "UoM": "VV" },         
+         */ 
+
     ]
     var json = [];
     
@@ -82,10 +136,11 @@ function push2QMiner(data) {
     }];
 
     // var res = request("GET", "http://localhost:9201/data/add-measurement?data=" + JSON.stringify(node));
-    //http.request("http://localhost:9201/data/add-measurement?data=" + JSON.stringify(node));
+    // http.request("http://localhost:9201/data/add-measurement?data=" + JSON.stringify(node));
+    shm.reqMade();
     request2("http://localhost:9201/data/add-measurement?data=" + JSON.stringify(node), function (error, response, body) {
-        console.log("test");
-        console.log(error);
+        resReceived();        
+        console.error(error.stack);
     });
     
         
@@ -98,24 +153,32 @@ function push2QMiner(data) {
 var zeroTS = db.zeroTS;
 var lastTS = db.lastTS;
 
-while (lastTS < (Date.now() / 1000)) {
-    lastTS = parseInt(lastTS) + parseInt(db.interval);
 
-    console.log("++ REQUEST: " + zeroTS + "-" + lastTS);
+// scheduler for 
+while (lastTS < (Date.now() / 1000)) {    
 
-    var res = request("GET", "http://193.2.205.65:5000/ami_retrieve?start=" + zeroTS + "&end=" + lastTS + "&num=-1");
-    data = JSON.parse(res.getBody())["query_result"];
-    // console.log(data);
-    for (var i = data.length - 1; i > 0; i--) {
-        // push data synchronously to QMiner instance
-        push2QMiner(data[i]);        
+    try {
+        lastTS = parseInt(lastTS) + parseInt(db.interval);
+        console.log("++ REQUEST: " + zeroTS + "-" + lastTS);
+        
+        var res = request("GET", "http://193.2.205.65:5000/ami_retrieve?start=" + zeroTS + "&end=" + lastTS + "&num=-1");
+        data = JSON.parse(res.getBody())["query_result"];
+        // console.log(data);
+        for (var i = data.length - 1; i > 0; i--) {
+            // push data synchronously to QMiner instance
+            push2QMiner(data[i]);
+        }
+        
+        if (data.length > 0) {
+            zeroTS = lastTS;
+        }
+        
+        db.update(zeroTS, lastTS);
+    } catch (err) {
+        // make lastTS back
+        lastTS = parseInt(lastTS) - parseInt(db.interval);
+        console.error(err.stack);
     }
-
-    if (data.length > 0) {
-        zeroTS = lastTS;
-    }
-
-    db.update(zeroTS, lastTS);
 
 }
 
