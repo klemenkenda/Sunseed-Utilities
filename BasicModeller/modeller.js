b0759b03f087ec4b9fc027ab5c48deaf934cf874
Klemen Kenda <klemen.kenda@ijs.si>
2016-07-13 06:45:37 +0200

    Prediction per sensor implemented.

--- BasicModeller/modeller.js
+++ BasicModeller/modeller.js
@@ -32,88 +32,92 @@ Date.prototype.addHours = function (h) {
 // initial load
 // http://atena.ijs.si/api/get-measurements?p=000137187-Consumed%20real%20power-pc1:2015-02-11:2016-03-20
 
-var tomorrow = new Date();
-tomorrow.setDate(tomorrow.getDate() + 1);
-
-var dayAfterTomorrow = new Date();
-dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 2);
-
-console.log(tomorrow.toMysqlDateFormat());
-
-console.log("Reading sensor data");
-var res = syncRequest('GET', 'http://atena.ijs.si/api/get-measurements?p=000137187-Consumed%20real%20power-pc1:2015-10-01:' + tomorrow.toMysqlDateFormat());
-var sensorData = JSON.parse(res.getBody());
-
-console.log("Reading holiday");
-var res = syncRequest('GET', 'http://atena.ijs.si/api/get-measurements?p=holiday:2015-10-01:' + dayAfterTomorrow.toMysqlDateFormat());
-var holiday = JSON.parse(res.getBody());
-
-console.log("Resampling sensor data");
-var lastTs;
-var lastValue = 0;
-
-var sensor = [];
-
-for (var i in sensorData) {
-    var ts = new Date(Date.parse(sensorData[i].Timestamp));
-    if (lastTs === undefined) {
-        lastTs = ts;
-    }            
-    // next expected
-    var nextTs = new Date(lastTs);
-    nextTs.addHours(1);
+function makePrediction(predictSensor) {    
+    var tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
     
-    console.log("next: " + nextTs.toMysqlFormat() + "; this: " + ts.toMysqlFormat());
+    var dayAfterTomorrow = new Date();
+    dayAfterTomorrow.setDate(dayAfterTomorrow.getDate() + 2);
     
-    if (nextTs.toMysqlFormat() == ts.toMysqlFormat()) {
-        console.log("OK");
-        lastTs = nextTs;
-        lastValue = sensorData[i].Val;
-        value = lastValue;
-        sensor.push({
-            Timestamp: nextTs,
-            Val: value,
-            Flag: 1
-        })
-    } else if (ts > nextTs) {
-        while (ts > nextTs) {
-            console.log("Error - missing: " + nextTs.toMysqlFormat());
-            var copy = new Date;
-            copy.setTime(nextTs.getTime());
-            sensor.push({
-                Timestamp: copy,
-                Val: lastValue,
-                Flag: 0
-            });
-            nextTs.addHours(1);
+    console.log(tomorrow.toMysqlDateFormat());
+    
+    console.log("Reading sensor data");
+    var res = syncRequest('GET', 'http://atena.ijs.si/api/get-measurements?p=' + escape(predictSensor) + ':2015-10-01:' + tomorrow.toMysqlDateFormat());
+    var sensorData = JSON.parse(res.getBody());
+    
+    console.log("Reading holiday");
+    var res = syncRequest('GET', 'http://atena.ijs.si/api/get-measurements?p=holiday:2015-10-01:' + dayAfterTomorrow.toMysqlDateFormat());
+    var holiday = JSON.parse(res.getBody());
+    
+    console.log("Resampling sensor data");
+    var lastTs;
+    var lastValue = 0;
+    
+    var sensor = [];
+    
+    for (var i in sensorData) {
+        var ts = new Date(Date.parse(sensorData[i].Timestamp));
+        if (lastTs === undefined) {
+            lastTs = ts;
         }
-        nextTs.addHours(-1);
-        lastTs = nextTs;
-    } else {
+        // next expected
+        var nextTs = new Date(lastTs);
+        nextTs.addHours(1);
+        
+        console.log("next: " + nextTs.toMysqlFormat() + "; this: " + ts.toMysqlFormat());
+        
+        if (nextTs.toMysqlFormat() == ts.toMysqlFormat()) {
+            console.log("OK");
+            lastTs = nextTs;
+            lastValue = sensorData[i].Val;
+            value = lastValue;
+            sensor.push({
+                Timestamp: nextTs,
+                Val: value,
+                Flag: 1
+            })
+        } else if (ts > nextTs) {
+            while (ts > nextTs) {
+                console.log("Error - missing: " + nextTs.toMysqlFormat());
+                var copy = new Date;
+                copy.setTime(nextTs.getTime());
+                sensor.push({
+                    Timestamp: copy,
+                    Val: lastValue,
+                    Flag: 0
+                });
+                nextTs.addHours(1);
+            }
+            nextTs.addHours(-1);
+            lastTs = nextTs;
+        } else {
         // just ignore
+        }
+        lastValue = sensorData[i].Val;
     }
-    lastValue = sensorData[i].Val;
-}
-
-
-console.log("MA PREDICTIONS");
-// find index of 1. 4. 2016
-var start = new Date(2016, 3, 1, 0, 0, 0);
-var offset = (start.getTime() - sensor[0].Timestamp.getTime()) / 1000 / 3600;
-var last = sensor[sensor.length - 1];
-var lastTime = new Date;
-lastTime.setTime(last.Timestamp.getTime() + 24 * 3600 * 1000);
-
-
-var currentTimestamp = new Date();
-currentTimestamp.setTime(sensor[offset].Timestamp.getTime());
-
-while (currentTimestamp < lastTime) {
-    currentTimestamp.addHours(1);
-    var virtualOffset = (currentTimestamp.getTime() - sensor[0].Timestamp.getTime()) / 1000 / 3600;
-    var prediction = calculateMA(virtualOffset, sensor, 5);    
-    console.log("Time: " + currentTimestamp.toMysqlFormat() + ", Prediction: " + prediction);
-    insertPrediction("000137187-Consumed real power-pc1", "ma", currentTimestamp.toMysqlFormat(), prediction);
+    
+    
+    console.log("MA PREDICTIONS");
+    // find index of 11. 4. 2016
+    var start = new Date(2016, 3, 30, 0, 0, 0);
+    var offset = (start.getTime() - sensor[0].Timestamp.getTime()) / 1000 / 3600;
+    var last = sensor[sensor.length - 1];
+    var lastTime = new Date;
+    lastTime.setTime(last.Timestamp.getTime() + 24 * 3600 * 1000);
+    
+    var currentTimestamp = new Date();
+    console.log(offset);
+    console.log(sensor.length);
+    
+    currentTimestamp.setTime(sensor[offset].Timestamp.getTime());
+    
+    while (currentTimestamp < lastTime) {
+        currentTimestamp.addHours(1);
+        var virtualOffset = (currentTimestamp.getTime() - sensor[0].Timestamp.getTime()) / 1000 / 3600;
+        var prediction = calculateMA(virtualOffset, sensor, 5);
+        console.log("Time: " + currentTimestamp.toMysqlFormat() + ", Prediction: " + prediction);
+        insertPrediction(predictSensor, "ma", currentTimestamp.toMysqlFormat(), prediction);
+    }
 }
 
 function insertPrediction(name, method, time, value) {
@@ -121,10 +125,10 @@ function insertPrediction(name, method, time, value) {
     sql += " ON DUPLICATE KEY UPDATE pr_value = " + value;
     
     console.log(sql);
-
+    
     connection.query(sql, function (err, rows) {
         if (err) console.log(err);
-    });
+    });    
 }
 
 function calculateMA(virtualOffset, sensor, N) {
@@ -147,4 +151,13 @@ function calculateMA(virtualOffset, sensor, N) {
     var prediction = sum / N;
 
     return prediction;
+}
+
+var sensors = [
+   "175339 Avtocenter ABC Kromberk 98441643-Consumed real power-pc"
+]
+
+for (var i in sensors) {
+    console.log(sensors[i]);
+    makePrediction(sensors[i]);
 }
