1b014337e071d41facfeed84c3a511d1f1b07e39
Load Forecasting <loadforecasting@sunseed-analytics.ijs.si>
2016-11-09 11:18:25 +0100

    Testing RabbitMQ - does VPN work?

--- BasicModeller/test-rabbit.js
+++ BasicModeller/test-rabbit.js
@@ -1,21 +1,28 @@
 var amqp = require('amqplib/callback_api');
 
 // seveda dosegljivo samo z VPN oz. znotraj 
-var host = "amqp://bpl-sunrabbit1.ts.telekom.si" 
+var host = "amqp://10.122.248.36" // bpl-sunrabbit1.ts.telekom.si
 var exchange = "lf_exchange"
 var queue_name = "lf_save_queue"
 var routing_key = "load_forecasting"
 
-amqp.connect(host, function(err, conn) {
-  if (err) throw (err);
-    
-  conn.createChannel(function(err, ch) {
-    var msg = 'Hello World!';
-    
-    ch.assertExchange(exchange, 'direct', {durable: false});
-    ch.publish(exchange, routing_key, new Buffer(msg));
-    console.log(" [x] Sent %s: '%s'", routing_key, msg);
-  });
+console.log("Connecting to host");
 
-  setTimeout(function() { conn.close(); process.exit(0) }, 500);
+amqp.connect(host, function(err, conn) {
+    if (err) throw (err);
+    
+    console.log("Creating channel!");
+    conn.createChannel(function(err, ch) {
+        var msg = 'Hello World!';
+        console.log("Starting Exchange");
+        ch.assertExchange(exchange, 'direct', {durable: false});
+        console.log("Publishing");
+        ch.publish(exchange, routing_key, new Buffer(msg));
+        console.log(" [x] Sent %s: '%s'", routing_key, msg);
+    });
+
+    setTimeout(function() { 
+        console.log("Closing");
+        conn.close(); process.exit(0) 
+    }, 500);
 });
