f15d38602daa01d63b5f97d44c4a73495e22dd65
Klemen Kenda <klemen.kenda@ijs.si>
2016-11-16 10:49:40 +0100

    Loading tweak, basic modeller for RMQ.

--- BasicModeller/PredictionInterface.js
+++ BasicModeller/PredictionInterface.js
@@ -10,6 +10,7 @@ function PredictionInterface(type) {
     this.pool;
     
     if (this.type == "mysql") this.connectMySQL();
+    if (this.type == "rabbitmq") this.connectRMQ();
 }
 
 PredictionInterface.prototype.connectMySQL = function() {
@@ -27,10 +28,34 @@ PredictionInterface.prototype.connectMySQL = function() {
 }
 
 
+PredictionInterface.prototype.connectRMQ = function() {
+   amqp.connect(Credentials.RMQhost, function(err, conn) {
+        if (err) throw (err);
+
+        console.log("Creating RabbitMQ channel!");
+        conn.createChannel(function(err, ch) {            
+            console.log("Starting Exchange");
+            ch.assertExchange(exchange, 'topic', {durable: true});
+        });
+    }); 
+}
+
+
+PredictionInterface.prototype.close = function() {
+    if (this.type == "rabbitmq") this.closeRMQ();
+}
+
+
+PredictionInterface.prototype.closeRMQ = function() {
+    console.log("Closing RabbitMQ connection.");
+    conn.close();
+}
+
 // insert prediction
 PredictionInterface.prototype.insertPrediction = function(name, method, time, value) {
     if (this.type == "mysql") this.insertPredictionMySQL(name, method, time, value);
     else if (this.type == "api") this.insertPredictionAPI(name, method, time, value);
+    else uf (this.type == "rabbitmq") this.insertPredictionRMQ(name, method, time, value);
     else console.debug("Wrong type: " + this.type);
 }
 
@@ -58,10 +83,26 @@ PredictionInterface.prototype.insertPredictionAPI = function(name, method, time,
 PredictionInterface.prototype.flushPredictions = function() {
     if (this.type == "mysql") console.debug("No flush needed for MySQL type.");
     else if (this.type == "api") this.flushPredictionsAPI();
+    else if (this.type == "rabbitmq") this.flushPredictionsRMQ();
     else console.debug("Wrong type: " + this.type);
 }
 
 PredictionInterface.prototype.flushPredictionsAPI = function() {
+    // send data to API (TODO)
+    var data2Send = "data=" + JSON.stringify(this.data);
+    console.log(data2Send);
+    var res = syncRequest("POST", credentials.apiURL, {
+        headers: {
+           "Content-Type": "application/x-www-form-urlencoded"  
+        },
+        body: data2Send
+    });
+    console.log(res.getBody().toString());
+    // upon successful submit - clear data
+    this.data = [];
+}
+
+PredictionInterface.prototype.flushPredictionsRMQ = function() {
     // send data to API (TODO)
     var data2Send = "data=" + JSON.stringify(this.data);
     console.log(data2Send);
