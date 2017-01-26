1ed1abf10c7425a4903909990a16ef4a7eb73dea
Klemen Kenda <klemen.kenda@ijs.si>
2017-01-26 23:25:28 +0100

    Node selection in GUI.

--- STLFModeller/sandbox/mqtt-connect.py
+++ STLFModeller/sandbox/mqtt-connect.py
@@ -2,13 +2,27 @@
 
 import paho.mqtt.client as mqtt
 
+topics = [];
+global n;
+n = 1;
+
 def on_connect(client, userdata, rc):
     print("Connected with result code "+str(rc))
     #client.subscribe("spm/+")
-    client.subscribe("spm/167002045410006104bfa000a0000094")
+    client.subscribe("spm/+")
 
 def on_message(client, userdata, msg):
-    print "Topic: ", msg.topic+'\nMessage: '+msg.payload
+    # print "Topic: ", msg.topic+'\nMessage: '+msg.payload
+    
+    if (n > 500):
+        print("Stop")
+        client.loop_stop()
+    if not (msg.topic in topics):
+        topic = msg.topic[4:]
+        topics.append(msg.topic)
+        print "Topic:", topic
+        with open("wams.txt", "a") as myfile:
+            myfile.write(topic + "\n")
 
 client = mqtt.Client()
 client.on_connect = on_connect
